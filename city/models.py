d7a3566a8fbfbe4750d4332eb2e2983b42e01184
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2026-03-23 19:15:17 +0000

    Add UUID field to City model and update city selection in templates: change city reference to use IDs in dropdowns for improved data handling and consistency.

--- city/models.py
+++ city/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+import uuid
 class City(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, blank=True, null=True)
     name = models.CharField(max_length=150, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
