1df56d5f452ec68e70e739a8e7280c8853c4bd45
amadoukoulibaly <amadou.koulibaly@jangogroup.com>
2026-03-29 16:34:23 +0000

    Update terminology in templates: replace instances of 'wallet' with 'compte' for consistency across expense and pump-related views, and adjust related alert messages accordingly.

--- station/settings.py
+++ station/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-#6g0k7uotj2!o8x#suuzqm%zy4qnoc(fxh48-n1vxi*q48=6-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['station.ets-myd.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
