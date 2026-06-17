e8eb01ea9dc6918f5a9fc9e955e1559fbe72c647
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2026-06-17 00:11:34 +0000

    Ajout de la gestion des champs numériques dans les formulaires : mise à jour des types d'input pour respecter les règles de saisie, intégration des classes `js-number-input` et `js-decimal-input`, et implémentation de la fonction `stripNumberInputsForSubmit` pour le traitement des données avant soumission. Amélioration de l'expérience utilisateur avec un formatage cohérent des montants.

---
+++ entry/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Entry(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, blank=True, null=True, unique=True, editable=False)
+    account = models.ForeignKey('wallet.Account', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    motif = models.TextField(null=True, blank=True)
+    date = models.DateField()
+    recorded_by = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "entries"
+
+    def __str__(self):
+        return f"Entry #{self.id} - {self.amount} {self.currency}"
